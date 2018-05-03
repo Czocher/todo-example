dbe66cbc1c965e58f6fc0f684cabeaa37fa4ff55
Paweł Jan Czochański <czochanski@gmail.com>
2018-05-04 00:44:36 +0200

    Add initial Vue frontend

---
+++ frontend/todo-app-vue/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="todo in todos" :key='todo.order'>
+      {{ todo.title }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data: function() {
+    return {
+      todos: [
+        { order: 0, title: "Alamakota", body: "Lotem ipsum" },
+        { order: 1, title: "Alamakota2", body: "Lotem ipsum2" }
+      ]
+    }
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
